0f0d31f1949847f2e884103d5c0cceafe378e172
João Ferreira <livingfromtheoutsidein@gmail.com>
2024-01-26 17:40:03 +0000

    Update database URL
    - Change file name in the database URL to a more appropriate name.
    - Fix a typo in the variable name.

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABSE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/hub.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABSE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(engine)
